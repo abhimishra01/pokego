a7c525724c390903f07fe7377c8347f50c195bc7
Abhishek Mishra <abhisk.mhra@gmail.com>
2022-07-24 15:28:57 +0530

    chore: add constants.js for maintaining constant vars

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const POKEMON_TYPES = {
+  ELECTRIC: "electric",
+  GRASS: "grass",
+  WATER: "water",
+  FIRE: "fire",
+  NORMAL: "normal",
+};
---
+++ src/utils/themes/colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+  primaryRed: "#f19686",
+  secondaryRed: "#d14d36",
+  electric: "#ECC948",
+  water: "#1694F0",
+  grass: "#51A548",
+  fire: "#DF2620",
+  normal: "#EBE2BC",
+};

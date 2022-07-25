fc7d097760f99ec623636b0221ee4e7f62e55507
Abhishek Mishra <abhisk.mhra@gmail.com>
2022-07-25 09:48:42 +0530

    feat: PokeStat component

---
+++ src/components/PokeStat/index.jsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+import { colors } from "utils/themes/colors";
+import { Slider } from "components/Slider";
+import { SLIDER_RANGE_ARRAY } from "utils/constants";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  justify-content: space-evenly;
+`;
+
+const StyledText = styled.span`
+  font-size: 1rem;
+  font-weight: 500;
+  color: ${colors.primaryWhite};
+`;
+
+const SliderContainer = styled.div`
+  margin: 0 1rem;
+  width: inherit;
+`;
+
+export const PokeStat = ({ statName, level }) => {
+  return (
+    <Container>
+      <StyledText>{statName}</StyledText>
+      <SliderContainer>
+        <Slider width={level} />
+      </SliderContainer>
+      <StyledText>{level}</StyledText>
+    </Container>
+  );
+};
+
+PokeStat.propTypes = {
+  statName: PropTypes.string.isRequired,
+  level: PropTypes.oneOf(SLIDER_RANGE_ARRAY.map((_, i) => i + 1)).isRequired,
+};
+
+PokeStat.defaultProps = {
+  level: 80,
+};

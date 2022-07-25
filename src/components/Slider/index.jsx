54babe8c3869a2a1124259d4c383e6ec553b555b
Abhishek Mishra <abhisk.mhra@gmail.com>
2022-07-25 09:49:18 +0530

    styles: update few stylings and prop types in slider comp

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -2,12 +2,14 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 
 import { colors } from "utils/themes/colors";
+import { SLIDER_RANGE_ARRAY } from "utils/constants";
 
 const Container = styled.div`
   width: 100%;
   background-color: ${colors.primaryRed};
   color: ${colors.primaryRed};
   position: relative;
+  max-height: 1rem;
 `;
 
 const StyledSpan = styled.span`
@@ -16,23 +18,23 @@ const StyledSpan = styled.span`
   left: 0;
   min-height: 100% !important;
   background-color: ${colors.secondaryRed};
-  color: ${colors.secondaryRed};
-  width: ${(props) => props.width};
+  color: ${colors.primaryRed};
+  width: ${(props) => props.width}%;
 `;
 
 export const Slider = ({ width }) => {
   return (
     <Container>
       Stats Level
-      <StyledSpan width={width}>{width}</StyledSpan>
+      <StyledSpan width={width}></StyledSpan>
     </Container>
   );
 };
 
 Slider.propTypes = {
-  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  width: PropTypes.oneOf(SLIDER_RANGE_ARRAY.map((_, i) => i + 1)).isRequired,
 };
 
 Slider.defaultProps = {
-  width: "50%",
+  width: 50,
 };
